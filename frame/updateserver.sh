31e5b48ca4d8ffd7950c551f339296880ff00a9a
FrancesShih <shiyongxin_327@aliyun.com>
2015-10-09 11:45:42 +0800

    ci adjust

--- frame/updateserver.sh
+++ frame/updateserver.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
-## remote update
-tar cfj code.bz2 ../web/*
-## scp code.zip root@182.92.111.197:/doc/docx
+### 打包
+`zip -r code.zip ./web/*`
+scp code.zip www@182.92.111.197:/doc/docx
+rm code.zip
+ssh www@182.92.111.197 "cd /doc/docx/; mv web webbk; unzip code.zip; rm code.zip;"
+echo "done"

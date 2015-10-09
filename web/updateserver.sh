a9f37ecdfcc7061dcf934b5d31cc265d6a559063
FrancesShih <shiyongxin_327@aliyun.com>
2015-10-09 23:45:47 +0800

    ci updateserver sh

--- web/updateserver.sh
+++ web/updateserver.sh
@@ -4,5 +4,5 @@
 `zip -r code.zip ./web/*`
 scp code.zip www@182.92.111.197:/doc/docx
 rm code.zip
-ssh www@182.92.111.197 "cd /doc/docx/; mv web webbk; unzip code.zip; rm -rf code.zip;"
+ssh www@182.92.111.197 "cd /doc/docx/; rm -rf web; unzip code.zip; rm -rf code.zip;"
 echo "done"
